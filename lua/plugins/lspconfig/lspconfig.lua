4bdca86483186409b5f619bee864706b8666c782
Eugene Nguyen <nguyendinhnhattai.it@gmail.com>
2023-11-30 00:41:56 +0700

    Omnisharp with Nix pkgs manager

--- lua/plugins/lspconfig/lspconfig.lua
+++ lua/plugins/lspconfig/lspconfig.lua
@@ -13,7 +13,7 @@ for type, icon in pairs(diagnostic_signs) do
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
 	-- NOTE: Remember that lua is a real programming language, and as such it is possible
 	-- to define small helper and utility functions so you don't have to repeat yourself
 	-- many times.
@@ -27,6 +27,83 @@ local on_attach = function(_, bufnr)
 
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 	end
+
+  if client.name == "omnisharp" then
+    client.server_capabilities.semanticTokensProvider = {
+          full = vim.empty_dict(),
+          legend = {
+            tokenModifiers = { "static_symbol" },
+            tokenTypes = {
+              "comment",
+              "excluded_code",
+              "identifier",
+              "keyword",
+              "keyword_control",
+              "number",
+              "operator",
+              "operator_overloaded",
+              "preprocessor_keyword",
+              "string",
+              "whitespace",
+              "text",
+              "static_symbol",
+              "preprocessor_text",
+              "punctuation",
+              "string_verbatim",
+              "string_escape_character",
+              "class_name",
+              "delegate_name",
+              "enum_name",
+              "interface_name",
+              "module_name",
+              "struct_name",
+              "type_parameter_name",
+              "field_name",
+              "enum_member_name",
+              "constant_name",
+              "local_name",
+              "parameter_name",
+              "method_name",
+              "extension_method_name",
+              "property_name",
+              "event_name",
+              "namespace_name",
+              "label_name",
+              "xml_doc_comment_attribute_name",
+              "xml_doc_comment_attribute_quotes",
+              "xml_doc_comment_attribute_value",
+              "xml_doc_comment_cdata_section",
+              "xml_doc_comment_comment",
+              "xml_doc_comment_delimiter",
+              "xml_doc_comment_entity_reference",
+              "xml_doc_comment_name",
+              "xml_doc_comment_processing_instruction",
+              "xml_doc_comment_text",
+              "xml_literal_attribute_name",
+              "xml_literal_attribute_quotes",
+              "xml_literal_attribute_value",
+              "xml_literal_cdata_section",
+              "xml_literal_comment",
+              "xml_literal_delimiter",
+              "xml_literal_embedded_expression",
+              "xml_literal_entity_reference",
+              "xml_literal_name",
+              "xml_literal_processing_instruction",
+              "xml_literal_text",
+              "regex_comment",
+              "regex_character_class",
+              "regex_anchor",
+              "regex_quantifier",
+              "regex_grouping",
+              "regex_alternation",
+              "regex_text",
+              "regex_self_escaped_character",
+              "regex_other_escape",
+            },
+          },
+          range = true,
+        }
+  end
 
 	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
@@ -59,10 +136,17 @@ local on_attach = function(_, bufnr)
 	end, { desc = "Format current buffer with LSP" })
 end
 
+local omnisharp_bin = "/nix/store/rvx89hrdnysz1zcdipcpnig63cmz71ys-omnisharp-roslyn-1.39.10/bin/OmniSharp";
+local pid = vim.fn.getpid()
+
 lspconfig.omnisharp.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
-	filetypes = { "cs", "vb" },
+	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+	enable_roslyn_analysers = true,
+	enable_import_completion = true,
+	organize_imports_on_format = true,
+	filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props" },
 	root_dir = util.root_pattern("*.sln", "*.csproj", "omnisharp.json", "function.json"),
 	settings = {
 		FormattingOptions = {
